677bbeb705c1a0687530df34b347c0daf2995fc0
Vansergus <56320602+Vansergus@users.noreply.github.com>
2019-10-16 23:21:04 +0500

    dobavil

---
+++ for zadanie #3/for zadanie 3.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+#include <string>
+#include <math.h>
+
+
+int main()
+{
+	int z, n;
+	std::cout << "vedite cislo: ";
+	std::cin >> n;
+	for (int i = 1; i <= n; i++) {
+		std::cout << i;
+		if ( i < n) {
+			std::cout << ",";
+		}
+	
+	}
+	return 0;
+}
+
