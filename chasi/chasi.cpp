01e53dd0953cf1a4bd59793c0339d28a4dee41f8
Vansergus <56320602+Vansergus@users.noreply.github.com>
2019-10-17 00:02:10 +0500

    pzdc

--- chasi/chasi.cpp
+++ chasi/chasi.cpp
@@ -9,8 +9,8 @@ int main()
 	std::cin >> n;
 
 	for (int i = n; i > 0; i--) {
-		for (int k = 0; k < i; k++) {
-			std::cout << " ";
+		for (int k = n-i; k > 0; k--) {
+			std::cout << "+";
 		}
 		for (int j = 0; j < i; j++) {
 			std::cout << "*";
@@ -19,15 +19,13 @@ int main()
 	}
 
 	for (int i = 2; i <= n; i++) {
-		for (int k = i; k > 0; k--) {
-			std::cout << " ";
+		for (int k = n - i; k > 0; k--) {
+			std::cout << "+";
 		}
 		for (int j = 0; j < i; j++) {
-			
-			std::cout << "*" ;
-			
+
+			std::cout << "*";
 		}
-		
 		std::cout << std::endl;
 	}
 	return 0;
