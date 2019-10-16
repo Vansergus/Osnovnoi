bcfe14592bce67cf38b1d64162261bf28a1b611a
Vansergus <56320602+Vansergus@users.noreply.github.com>
2019-10-16 23:23:28 +0500

    perezalil

---
+++ ConsoleApplication #5/ConsoleApplication5.cpp
@@ -0,0 +1,30 @@
+﻿// ConsoleApplication5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+#include <math.h>
+
+int main()
+{
+	int n;
+	std::cout << "vedite znacenie n: ";
+	std::cin >> n;
+
+	for (int i = n; i > 0; i--) {
+		for (int j = 0; j < i; j++) {
+			std::cout << "*";
+		}
+		std::cout << std::endl;
+	}
+
+	for (int i = 2; i <= n; i++) {
+		for (int j = 0; j < i; j++) {
+			std::cout << "*";
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+
+}
+
