8377741e84f901367d65064d5eec8f2a61070220
Vansergus <56320602+Vansergus@users.noreply.github.com>
2019-10-28 20:03:56 +0500

    dobavil chasi  i rvadrat s pustotoi

---
+++ chasi 2/chasi 2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <math.h>
+
+int main()
+{
+	int n;
+	std::cout << "vedite znacenie n: ";
+	std::cin >> n;
+
+	for (int i = n; i > 0; i--) {
+		for (int k = n-i; k >0; k--) {
+			std::cout << "+";
+			
+		}
+		for (int j = n-i; j < i; j++) {
+			std::cout << "*";
+		}
+		
+		std::cout << std::endl;
+	}
+
+	for (int i = 2; i <= n; i++) {
+		for (int k = n - i; k >0; k--) {
+			std::cout << "+";
+		}
+		for (int j =n-i; j < i; j++) {
+
+			std::cout << "*";
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+
+}
--- chasi/chasi.cpp
+++ chasi/chasi.cpp
@@ -8,26 +8,27 @@ int main()
 	std::cout << "vedite znacenie n: ";
 	std::cin >> n;
 
-	for (int i = n; i > 0; i--) {
-		for (int k = n-i; k > 0; k--) {
+	for (int i = n; i-1 > 0; i-=2) {
+		for (int k = n-i; k > 0; k-=2) {
 			std::cout << " ";
 		}
-		for (int j = 0-i; j < i; j++) {
+		for (int j = 0; j < i; j++) {
 			std::cout << "*";
 		}
 		std::cout << std::endl;
 	}
 
-	for (int i = 2; i <= n; i++) {
-		for (int k = n - i; k > 0; k--) {
+	for (int i = 0; i < n; i+=2) {//УПРАВЛЕНИЕ СТРОКАМИ
+		for (int z = n-i; z > 2; z-=2) {
 			std::cout << " ";
 		}
-		for (int j = 0-i; j < i; j++) {
+		for (int j = 0; j <=i; j++) {
 
 			std::cout << "*";
 		}
 		std::cout << std::endl;
 	}
+
 	return 0;
 
 }
