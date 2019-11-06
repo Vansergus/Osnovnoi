dff248b336acdf9124ae5c882fbceea2fab0c7cc
Vansergus <56320602+Vansergus@users.noreply.github.com>
2019-11-06 21:08:05 +0500

    добавлен проект

---
+++ задание 29октябрь/задание 29октябрь.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <math.h>
+
+int main()
+{
+	int n;
+    std::cout << "vedite chislo ";
+	std::cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			/*if (i==n-j-1||i==j)*/
+			/*if (n-i-1<=j&&i<=j||j<=i&&n-i>j)*/
+			if(i<=j&&n-i>j||j<=i&&j>n-i-2){
+				std::cout << "*";
+			}
+			else {
+				std::cout << "-";
+			}
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
+
